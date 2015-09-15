47618c59022001e2f15723b99402efe3e21beec0
andosteinmetz <ando.steinmetz@gmail.com>
2015-09-15 18:22:14 +0000

    configured GPIO, not working

--- matrix_keypad.py
+++ matrix_keypad.py
@@ -22,16 +22,18 @@ class Keypad:
     def __init__(self, columnPins, rowPins):
         self.cols = columnPins
         self.rows = rowPins
+        self.colCount = len(columnPins)
+        self.rowCount = len(rowPins)
         self.setup()
         
     def setup(self):
         #set columns as low output
-        for i in range(len(self.cols)):
+        for i in range(self.colCount):
             pig.set_mode(self.cols[i], pigpio.OUTPUT)
             pig.write(self.cols[i], 0)
 
         #set rows as input with pull up resistor
-        for j in range(len(self.rows)):
+        for j in range(self.rowCount):
             pig.set_mode(self.rows[j], pigpio.INPUT)
             pig.set_pull_up_down(self.rows[j], pigpio.PUD_UP)
 
@@ -39,47 +41,63 @@ class Keypad:
         self.setup()
         keyInput = []
         rowVal = -1
-        for i in range(len(self.rows)):
-            tmpRead = pigpio.read(self.rows[i])
+        for i in range(self.rowCount):
+            tmpRead = pig.read(self.rows[i])
             if tmpRead == 0:
                 rowVal = i
 
         # if rowVal is greater than the row count - 1 or less than zero, exit
-        if rowVal < 0 or rowVal > len(self.cols) -1:
-            self.exit()
-            return
-
-        #convert columns to input
-        for j in range(len(self.cols)):
-            col = self.cols[j]
-            pig.setmode(col, pigpio.INPUT)
-            pig.set_pull_up_down(col, pigpio.PUD_DOWN)
-
-        #Switch the i-th row found from scan to output
-        pigpio.set_mode(self.rows[rowVal], pigpio.OUTPUT)
-        pigpio.write(self.rows[rowVal], 1)
-
-        # Scan colums for still-pushed key
-        colVal = -1
-        for j in range(len(self.cols)):
-            tmpRead = pig.read(self.cols[j])
-            if tmpRead == 1:
-                colVal = j
-
-        if colVal < 0 or colVal > self.cols - 1:
+        if rowVal < 0 or rowVal > self.colCount -1:
             self.exit()
             return
 
         keyInput.append(rowVal)
+
+        #convert columns to input
+        for j in range(self.colCount):
+            col = self.cols[j]
+            pig.set_mode(col, pigpio.INPUT)
+            pig.set_pull_up_down(col, pigpio.PUD_DOWN)
+
+        #Switch the i-th row found from scan to output
+        pig.set_mode(self.rows[rowVal], pigpio.OUTPUT)
+        pig.write(self.rows[rowVal], 1)
+
+        # Scan colums for still-pushed key
+        colVal = -1
+        for j in range(self.colCount):
+            tmpRead = pig.read(self.cols[j])
+            if tmpRead == 1:
+                colVal = j
+
+        if colVal < 0 or colVal > self.colCount - 1:
+            self.exit()
+            return
+        
         keyInput.append(colVal)
 
     def exit(self):
-        for i in range(len(self.rows)):
-            pig.set_mode(self.row[i], pigpio.INPUT)
-            pig.set_pull_up_down(self.row[i], pigpio.PUD_UP)
-        for j in range(len(self.cols)):
-            pig.set_mode(self.cols[j], pigpio.INPUT, pigpio.PUD_UP)
-            
+        for i in range(self.rowCount):
+            pig.set_mode(self.rows[i], pigpio.INPUT)
+            pig.set_pull_up_down(self.rows[i], pigpio.PUD_UP)
+        for j in range(self.colCount):
+            pig.set_mode(self.cols[j], pigpio.INPUT)
+            pig.set_pull_up_down(self.cols[j], pigpio.PUD_UP)
 
         return input
                        
+if __name__ == '__main__':
+    #configure GPIO
+    myRows = [26,19,13,6]
+    myCols = [21,20,16,12]
+
+    #create an instance
+    kp = Keypad(myCols, myRows)
+
+    #Loop while waiting for a keypress
+    myInput = None
+    while myInput == None:
+        myInput = kp.getKey()
+
+    #print the result
+    print myInput
